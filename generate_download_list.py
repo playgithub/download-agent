81833ce0fa6a61db7b19dd94cc5f18d638e21989
abc <a@b.com>
2022-10-25 21:17:03 +0800

    rename

---
+++ generate_download_list.py
@@ -0,0 +1,21 @@
+import string
+import yaml
+
+with open('input.yml', 'r', encoding = 'utf-8') as f:
+    file_content = f.read()
+
+tempalte_file_content = string.Template(file_content)
+
+yaml_input = yaml.safe_load(file_content)
+versions = yaml_input['versions']
+
+file_content = tempalte_file_content.safe_substitute(versions)
+
+yaml_input = yaml.safe_load(file_content)
+
+items = yaml_input['items']
+
+with open('./aria2c_input.txt', 'w', encoding = 'utf-8') as f:
+    for item in items:
+        url_with_newline = item['url'] + '\n'
+        f.write(url_with_newline)
