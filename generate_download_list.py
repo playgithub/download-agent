049d0358aaa6988b55887ad2a65d8358be5568f2
abc <a@b.com>
2022-10-26 11:01:06 +0800

    refactor input.yml

--- generate_download_list.py
+++ generate_download_list.py
@@ -4,18 +4,10 @@ import yaml
 with open('input.yml', 'r', encoding = 'utf-8') as f:
     file_content = f.read()
 
-tempalte_file_content = string.Template(file_content)
-
 yaml_input = yaml.safe_load(file_content)
-versions = yaml_input['versions']
-
-file_content = tempalte_file_content.safe_substitute(versions)
-
-yaml_input = yaml.safe_load(file_content)
-
-items = yaml_input['items']
 
 with open('./aria2c_input.txt', 'w', encoding = 'utf-8') as f:
-    for item in items:
-        url_with_newline = item['url'] + '\n'
+    for item in yaml_input:
+        tempalte_url = string.Template(item['url'])
+        url_with_newline = tempalte_url.safe_substitute(item) + '\n'
         f.write(url_with_newline)
