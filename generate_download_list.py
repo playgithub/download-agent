87ea1089e78b4302d9594dd63c178e2fb858dd45
abc <a@b.com>
2022-10-26 12:11:46 +0800

    rename

--- generate_download_list.py
+++ generate_download_list.py
@@ -4,10 +4,10 @@ import yaml
 with open('input.yml', 'r', encoding = 'utf-8') as f:
     file_content = f.read()
 
-yaml_input = yaml.safe_load(file_content)
+items = yaml.safe_load(file_content)
 
 with open('./aria2c_input.txt', 'w', encoding = 'utf-8') as f:
-    for item in yaml_input:
-        tempalte_url = string.Template(item['url'])
-        url_with_newline = tempalte_url.safe_substitute(item) + '\n'
+    for item in items:
+        string_tempalte_url = string.Template(item['url'])
+        url_with_newline = string_tempalte_url.safe_substitute(item) + '\n'
         f.write(url_with_newline)
